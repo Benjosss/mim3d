3a74f8a16abf2e546ae8d4c72221972ed07ef574
Benjosss <b55.lallement@gmail.com>
2026-04-28 13:38:13 +0200

    [FEATURE] : Added routing to app

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'index.html'),
+                app: resolve(__dirname, 'src/app/app.html'),
             },
         },
     },
