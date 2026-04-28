d42fce2c6931fc354832e388edd53ab25ab7ba41
Benjosss <b55.lallement@gmail.com>
2026-04-28 13:36:08 +0200

    [FEATURE] : Added json parser

---
+++ src/utils/json-parser.js
@@ -0,0 +1,66 @@
+import {Zone} from "../map-manager/zone.js";
+import * as THREE from "three";
+
+export default class JsonParser {
+    /**
+     * Constructeur du parseur
+     */
+    constructor() {
+    }
+
+    /**
+     * Retourne les données parsées
+     * @param data
+     * @returns {*}
+     */
+    parse(data) {
+        return data;
+    }
+
+    /**
+     * Lit et parse le fichier de données
+     * @returns {Promise<any>}
+     */
+    fetchJSONData(filePath) {
+        return fetch(filePath)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`Erreur, statut : ${response.status}`);
+                }
+                return response.json();
+            })
+            .then(data => {
+                return this.parse(data);
+            })
+            .catch(error => {
+                console.error('Erreur lors de la récupération :', error);
+            });
+    }
+
+    async fillZonesTab(ZONES, path) {
+        const jsonData = await this.fetchJSONData(path);
+
+        if (!jsonData) {
+            console.log("Impossible de charger les zones");
+        } else {
+            jsonData.forEach((zone) => {
+                ZONES.push(
+                    new Zone({
+                        name: zone.name,
+                        path: zone.path,
+                        impostorPath: zone.impostorPath,
+                        physics: zone.physics,
+                        type: zone.type,
+                        description: zone.description,
+                        adjacentZoneNames: zone.adjacentZoneNames || [],
+                        triggerBox: new THREE.Box3(
+                            new THREE.Vector3(...zone.triggerBox.min),
+                            new THREE.Vector3(...zone.triggerBox.max)
+                        ),
+                    })
+                );
+            });
+        }
+        return ZONES;
+    }
+}
