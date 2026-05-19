9497532a7f9c7fb3deaf2ea05e283e8fed2da2b3
Benjosss <b55.lallement@gmail.com>
2026-05-19 09:22:59 +0200

    [FIX] : Added rebuild commands

--- admin/linux_modif_bureau.sh
+++ admin/linux_modif_bureau.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 node ./edit_offices.js
+echo.
+echo Déploiement de la mise à jour...
+cd ..
+npm run build
 read -p "Appuyez sur Entrée pour fermer..."
--- admin/windows_modif_bureau.bat
+++ admin/windows_modif_bureau.bat
@@ -1,4 +1,9 @@
 @echo off
 cd /d "%~dp0"
 node ./edit_offices.js
+
+echo.
+echo Déploiement de la mise à jour...
+cd ..
+npm run build
 pause
